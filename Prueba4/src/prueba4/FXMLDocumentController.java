f475551de0b7f4910b4d9167cbaf7d788e6d7f86
Rodrigo <rl44610@hotmail.com>
2019-08-01 16:39:42 -0400

    El boton desaparece

--- Prueba4/src/prueba4/FXMLDocumentController.java
+++ Prueba4/src/prueba4/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -20,11 +21,14 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button button;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        button.setVisible(false);
     }
     
     @Override
