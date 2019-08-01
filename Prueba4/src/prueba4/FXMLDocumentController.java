7a1b832e36b60263c00fff594f765b1a2d0a35a7
Rodrigo <rl44610@hotmail.com>
2019-08-01 17:02:59 -0400

    Agregar Boton de Comienzo

--- Prueba4/src/prueba4/FXMLDocumentController.java
+++ Prueba4/src/prueba4/FXMLDocumentController.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prueba4;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
 /**
@@ -19,13 +16,24 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private ImageView fondoNormal;
-    
-    
-    
+    @FXML
+    private Button BotonComienzo;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
+
+    @FXML
+    private void Comienzo(ActionEvent event) {
+        
+        fondoNormal.setVisible(true);
+        
+        BotonComienzo.setVisible(false);
+        
+        System.out.println("TUNAZOOOOOOO  ENE EL OCICO");
+                
+        
+    }
     
 }
