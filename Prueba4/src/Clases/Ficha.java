44473556c54285a3ddf103529055aa11eb1f63e0
Rodrigo <rl44610@hotmail.com>
2019-08-01 16:13:04 -0400

    Crear Clase Tablero y Clase Ficha

---
+++ Prueba4/src/Clases/Ficha.java
@@ -0,0 +1,29 @@
+package Clases;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Ficha {
+    
+    private int num;
+    private String nombre;
+    
+    public Ficha(int num, String nombre){
+        this.num = num;
+        this.nombre = nombre;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
+    
+    
+    
+}
---
+++ Prueba4/src/Clases/Tablero.java
@@ -0,0 +1,28 @@
+package Clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Tablero {
+    
+    private ArrayList adivinar; 
+    
+    public Tablero(ArrayList advinar){
+        this.adivinar = adivinar;
+    }
+
+    public ArrayList getAdivinar() {
+        return adivinar;
+    }
+
+    public void setAdivinar(ArrayList adivinar) {
+        this.adivinar = adivinar;
+    }
+    
+    
+    
+    
+}
